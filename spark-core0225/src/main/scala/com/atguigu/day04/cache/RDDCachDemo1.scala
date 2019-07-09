5b79548f11683b69f5182e8cc7e6eb9258fab54f
John Doe <30752740@qq.com>
2019-07-09 10:57:55 +0800

    更新

---
+++ spark-core0225/src/main/scala/com/atguigu/day04/cache/RDDCachDemo1.scala
@@ -0,0 +1,11 @@
+package com.atguigu.day04.cache
+
+/**
+  * Author lzc
+  * Date 2019-07-09 10:57
+  */
+object RDDCachDemo1 {
+    def main(args: Array[String]): Unit = {
+        
+    }
+}
---
+++ spark-core0225/src/main/scala/com/atguigu/day04/passfun/PassFun3.scala
@@ -0,0 +1,44 @@
+package com.atguigu.day04.passfun
+
+import org.apache.spark.{SparkConf, SparkContext}
+import org.apache.spark.rdd.RDD
+
+object PassFun3 {
+    def main(args: Array[String]): Unit = {
+        val conf: SparkConf = new SparkConf()
+            .setAppName("SerDemo")
+            .setMaster("local[*]")
+            .set("spark.serializer", "org.apache.spark.serializer.KryoSerializer")
+        //org.apache.spark.serializer.KryoSerializer
+            .registerKryoClasses(Array(classOf[Searcher1]))
+        
+        val sc = new SparkContext(conf)
+        val rdd: RDD[String] = sc.parallelize(Array("hello world", "hello atguigu", "atguigu", "hahah"), 2)
+        val searcher = new Searcher1("hello")
+        val result: RDD[String] = searcher.getMatchedRDD1(rdd)
+        result.collect.foreach(println)
+    }
+}
+//需求: 在 RDD 中查找出来包含 query 子字符串的元素
+
+// query 为需要查找的子字符串
+class Searcher1(val query: String) extends Serializable {
+    // 判断 s 中是否包括子字符串 query
+    def isMatch(s : String) ={
+        s.contains(query)
+    }
+    // 过滤出包含 query字符串的字符串组成的新的 RDD
+    def getMatchedRDD1(rdd: RDD[String]) ={
+        rdd.filter(isMatch)  //
+    }
+    // 过滤出包含 query字符串的字符串组成的新的 RDD
+    def getMatchedRDD2(rdd: RDD[String]) ={
+        rdd.filter(_.contains(query))
+    }
+    
+    def getMatchedRDD3(rdd: RDD[String]) ={
+        val q = this.query
+        rdd.filter(_.contains(q))
+    }
+}
+
